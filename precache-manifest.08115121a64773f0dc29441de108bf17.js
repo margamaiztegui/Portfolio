546f28d8e7255ffa0676123b231e5dbc413632eb
Margadoxx <margadoxx@MargadoxxMM.local>
2026-05-10 10:18:08 +0800

    Updates

---
+++ precache-manifest.08115121a64773f0dc29441de108bf17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecdf6758a71ccb15758e648d62a957fc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "764d548dd381603efdb3",
+    "url": "/portfolio/static/css/2.fd277b09.chunk.css"
+  },
+  {
+    "revision": "26b081ad4e12e3afd4f6",
+    "url": "/portfolio/static/css/main.d54f2a5a.chunk.css"
+  },
+  {
+    "revision": "764d548dd381603efdb3",
+    "url": "/portfolio/static/js/2.5e37e901.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/portfolio/static/js/2.5e37e901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b081ad4e12e3afd4f6",
+    "url": "/portfolio/static/js/main.f8df4f86.chunk.js"
+  },
+  {
+    "revision": "db8f5a02fc62b3e6ea7e",
+    "url": "/portfolio/static/js/runtime-main.4f7c1dcd.js"
+  }
+]);
